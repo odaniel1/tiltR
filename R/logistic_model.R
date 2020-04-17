d78ac03957cd2e0df84439d3ffb434d7f9d499c3
odaniel1 <owendnl@gmail.com>
2020-04-17 22:20:47 +0100

    simplify arguments

--- R/logistic_model.R
+++ R/logistic_model.R
@@ -4,8 +4,24 @@
 #' @param ... Arguments passed to `rstan::sampling` (e.g. iter, chains).
 #' @return An object of class `stanfit` returned by `rstan::sampling`
 #'
-logistic_model_stan <- function(N, t, sg, og, fg_ant, fg_sd, days, ...) {
-  standata <- list(N = N, t= t, sg = sg, og = og, fg_ant = fg_ant, fg_sd = fg_sd, days = days)
+# logistic_model_stan <- function(t, sg, og, fg_ant, fg_sd, days, ...) {
+#   standata <- list(N = , t= t, sg = sg, og = og, fg_ant = fg_ant, fg_sd = fg_sd, days = days)
+#   out <- rstan::sampling(stanmodels$logistic_model, data = standata, ...)
+#   return(out)
+# }
+
+logistic_model_stan <- function(data, pars = c("t", "sg"), og = NULL, fg_ant, fg_sd, days, ...) {
+
+  t  <- unlist(data[, pars[1] ])
+  sg <- unlist(data[, pars[2] ])
+
+  if(is.null(og)) og <- max(sg)
+
+  standata <- list(
+    N = length(t), t = t, sg = sg, og = og, fg_ant = fg_ant, fg_sd = fg_sd, days = days
+  )
+
   out <- rstan::sampling(stanmodels$logistic_model, data = standata, ...)
+
   return(out)
-}
+  }
